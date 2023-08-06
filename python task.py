4e89239f0e3964b95a1a24ac046aef49ea746200
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-08-06 12:15:38 +0300

    add new file

---
+++ python task.py
@@ -0,0 +1,12 @@
+x=15
+z=25
+y=47
+if x<=15:
+    print ('x elkbeeer')
+elif  x<=11:
+    print ('222222222222222220000')
+elif y==z+x+9:
+    print ('howwwwwwwwwww da')
+else:
+    print(x*z+y)
+    
